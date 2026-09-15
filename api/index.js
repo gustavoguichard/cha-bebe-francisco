81f6171a521ce21f25764e56c1a600ef4f56dc1c
Gustavo Guichard <566971+gustavoguichard@users.noreply.github.com>
2026-09-14 23:02:01 -0300

    Repasso o caminho original para a função da Vercel

--- api/index.js
+++ api/index.js
@@ -2,6 +2,12 @@ import { router } from '../dist/app/router.js'
 
 export default {
   fetch(request) {
-    return router.fetch(request)
+    let url = new URL(request.url)
+    let path = url.searchParams.get('__path')
+    if (path === null) return router.fetch(request)
+
+    url.searchParams.delete('__path')
+    url.pathname = '/' + path.replace(/^\/+/, '')
+    return router.fetch(new Request(url, request))
   },
 }
